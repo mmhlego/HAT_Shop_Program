ca914ac9b7fe9f89ba7fb2bd3d290eae80022969
pouyaafraz123 <pouyaafraz123@gmail.com>
2021-04-14 01:26:27 +0330

    load added to first Page

--- src/LoginPage/Controller/FirstPage.java
+++ src/LoginPage/Controller/FirstPage.java
@@ -1,3 +1,4 @@
+
 package LoginPage.Controller;
 
 import java.io.IOException;
@@ -10,8 +11,10 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -28,27 +31,72 @@ public class FirstPage implements Initializable, Runnable {
 	@FXML
 	private Label powerLBL;
 
+	@FXML
+	private ProgressIndicator connect;
+
+	@FXML
+	private Label connectLBL;
+	int i = 0;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		engilshLBL.setTranslateY(400);
-		persianLBL.setTranslateY(400);
-		powerLBL.setTranslateY(400);
+		engilshLBL.setTranslateY(550);
+		persianLBL.setTranslateY(550);
+		powerLBL.setTranslateY(550);
+		connect.setTranslateY(550);
+		connectLBL.setTranslateY(550);
 		Thread thread = new Thread(this);
 		thread.start();
 	}
 
-	private void firstPageTransition(Label label) {
+	private void firstPageTransition(Node label) {
 		TranslateTransition transition = new TranslateTransition();
-		transition.setByY(-400);
+		transition.setByY(-550);
 		transition.setNode(label);
 		transition.setCycleCount(1);
 		transition.setInterpolator(Interpolator.EASE_BOTH);
-		transition.setDuration(Duration.seconds(1.5));
+		transition.setDuration(Duration.seconds(1));
 		transition.play();
 		transition.setOnFinished(e -> {
 			if (label.equals(engilshLBL)) {
 				firstPageTransition(persianLBL);
 			} else if (label.equals(persianLBL)) {
+				firstPageTransition(connect);
+			} else if (label.equals(connect)) {
+				firstPageTransition(connectLBL);
+				Thread thread = new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+
+						while (true) {
+							try {
+								Thread.sleep(500);
+								Platform.runLater(new Runnable() {
+
+									@Override
+									public void run() {
+										if (i % 4 == 0) {
+											connectLBL.setText("در حال اتصال");
+										} else if (i % 4 == 1) {
+											connectLBL.setText("در حال اتصال .");
+										} else if (i % 4 == 2) {
+											connectLBL.setText("در حال اتصال ..");
+										} else if (i % 4 == 3) {
+											connectLBL.setText("در حال اتصال ...");
+										}
+										i++;
+									}
+								});
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+
+					}
+				});
+				thread.start();
+			} else if (label.equals(connectLBL)) {
 				firstPageTransition(powerLBL);
 			} else {
 				Thread thread = new Thread(new Runnable() {
@@ -56,7 +104,7 @@ public class FirstPage implements Initializable, Runnable {
 					@Override
 					public void run() {
 						try {
-							Thread.sleep(1000);
+							Thread.sleep(3000);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
