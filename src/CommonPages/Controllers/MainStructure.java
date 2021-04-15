2d304e07f30ee1b3282780cd95841c47f6fb1d72
pouyaafraz123 <pouyaafraz123@gmail.com>
2021-04-15 12:54:39 +0330

    main class and main structure

--- src/CommonPages/Controllers/MainStructure.java
+++ src/CommonPages/Controllers/MainStructure.java
@@ -5,9 +5,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -23,6 +25,12 @@ public class MainStructure implements Initializable {
 	private VBox SideBar;
 
 	@FXML
+	private VBox menu;
+
+	@FXML
+	private ImageView menuImg;
+
+	@FXML
 	private VBox imageBox;
 
 	@FXML
@@ -30,19 +38,28 @@ public class MainStructure implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		System.out.println(1);
 		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../Visual/OpenSide.fxml"));
+		menu.setCursor(Cursor.HAND);
+		imageBox.setCursor(Cursor.HAND);
 		try {
 			Parent root = loader.load();
 			OpenSide sideBarController = loader.getController();
 			MainPanel.getChildren().add(root);
 			root.setTranslateX(1080);
-			SideBar.setOnMouseClicked(e -> {
+			menu.setOnMouseClicked(e -> {
 				sideBarAnimaton(root);
 			});
+
+			imageBox.setOnMouseClicked(e -> Platform.exit());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	private void AddButton(String fxml, String name) {
+		// FXMLLoader loader = new
+		// FXMLLoader(this.getClass().getResource("../Visual/"));
 
 	}
 
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,45 @@
-import DataController.*;
+import DataController.DBConnector;
+import DataController.DataChecker;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.*;
+import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.stage.*;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 	public static void main(String[] args) {
-		DBConnector.Connect();
-
-		//DataAdder.AddManager("Kamyab", "Tabani", "CyberGhost", "HAT_Cyber", "0914655912", "K.tabani82@gmail.com");
-		//DataAdder.AddEmployee("Puya", "Afraz", "P.A", "VLB", 1, "09148303766", "Pouyaafraz@gmail.com", "E-0001");
-		//DataAdder.AddCustomer("MohammadMahdi", "Hejazi", "M.M.H", "M.M.H Lego", "09144571245", "mmhlego@gmail.com", "Sahand - kuye sahanad", 0, 1, "C-0001");
-		// System.out.println(DataChecker.UserExists("CyberGhost"));
-		// DataAdder.AddProduct("Samsung Galaxy A51", "Samsung", "Nothing", 10000000, 10, "Phone", 5, "P-0001");
-		System.out.println(DataChecker.GetAmountOf("Manager"));
-
 		launch(args);
+
+		// DataAdder.AddManager("Kamyab", "Tabani", "CyberGhost", "HAT_Cyber",
+		// "0914655912", "K.tabani82@gmail.com");
+		// DataAdder.AddEmployee("Puya", "Afraz", "P.A", "VLB", 1, "09148303766",
+		// "Pouyaafraz@gmail.com", "E-0001");
+		// DataAdder.AddCustomer("MohammadMahdi", "Hejazi", "M.M.H", "M.M.H Lego",
+		// "09144571245", "mmhlego@gmail.com", "Sahand - kuye sahanad", 0, 1, "C-0001");
+		// System.out.println(DataChecker.UserExists("CyberGhost"));
+		// DataAdder.AddProduct("Samsung Galaxy A51", "Samsung", "Nothing", 10000000,
+		// 10, "Phone", 5, "P-0001");
+
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		Thread thread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000);
+					System.out.println("Start Connecting ...");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(DBConnector.Connect());
+				System.out.println(DataChecker.GetAmountOf("Manager"));
+			}
+		});
+		thread.start();
 		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("LoginPage/View/FirstPage.fxml"));
 		Scene scene = new Scene(loader.load());
 		scene.setFill(Color.TRANSPARENT);
