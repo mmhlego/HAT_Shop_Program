113be8afb0254816f262d5333134d8ea649cccc5
mmhlego <mmhlego@gmail.com>
2021-04-26 10:42:57 +0430

    Added Cart Page

---
+++ src/Customer/Controller/Cart.java
@@ -0,0 +1,24 @@
+package Customer.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class Cart {
+    @FXML
+    private VBox ProductsListPanel;
+    @FXML
+    private Button FinalizeOrderBTN;
+    @FXML
+    private Label SumOfDiscountsLBL;
+    @FXML
+    private Label SumOfPricesLBL;
+    @FXML
+    private Label FinalPriceLBL;
+    @FXML
+    private Label ShippingDateLBL;
+    @FXML
+    private Label ShippingFeeLBL;
+
+}
