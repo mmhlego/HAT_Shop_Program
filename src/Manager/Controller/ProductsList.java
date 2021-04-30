8a28a735d8e2894446d4626edab5bdca5b0d4f50
mmhlego <mmhlego@gmail.com>
2021-04-30 17:32:01 +0430

    Added productslist to manager

---
+++ src/Manager/Controller/ProductsList.java
@@ -0,0 +1,23 @@
+package Manager.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class ProductsList implements Initializable {
+	@FXML
+	private TextField SearchTXF;
+	@FXML
+	private Button SearchBTN;
+	@FXML
+	private VBox MainPanel;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+}
