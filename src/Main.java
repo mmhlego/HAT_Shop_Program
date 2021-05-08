2cf74a57f3fbaaf97e82b0ad3c053617ccc9e4ac
mmhlego <mmhlego@gmail.com>
2021-05-08 10:17:42 +0430

    Fixed pom.xml + Main.java

--- src/Main.java
+++ src/Main.java
@@ -11,70 +11,70 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	private static void PrintEstimatedTime(long start) {
-		long time = System.currentTimeMillis() - start;
-		System.out.println("\n=====================================================================================\n");
-		System.out.println("Program run time information:");
-		System.out.println("\t- " + (time / 1000) / 60 + " Minutes");
-		System.out.println("\t- " + (time / 1000) % 60 + " Seconds");
-		System.out.println("\t- " + time % 1000 + " Milliseconds");
-		System.out.println("\n=====================================================================================\n");
-	}
+    private static void PrintEstimatedTime(long start) {
+        long time = System.currentTimeMillis() - start;
+        System.out.println("\n=====================================================================================\n");
+        System.out.println("Program run time information:");
+        System.out.println("\t- " + (time / 1000) / 60 + " Minutes");
+        System.out.println("\t- " + (time / 1000) % 60 + " Seconds");
+        System.out.println("\t- " + time % 1000 + " Milliseconds");
+        System.out.println("\n=====================================================================================\n");
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		DBConnector.stage = primaryStage;
-		Thread thread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				long start = System.currentTimeMillis();
-				try {
-					Thread.sleep(3000);
-				} catch (Exception e) {
-				}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        DBConnector.stage = primaryStage;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start = System.currentTimeMillis();
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                }
 
-				while (!DBConnector.Connect()) {
-					System.out.println("Disconnected. Trying again . . .");
-				}
+                while (!DBConnector.Connect()) {
+                    System.out.println("Disconnected. Trying again . . .");
+                }
 
-				System.out.println("Connected");
-				PrintEstimatedTime(start);
+                System.out.println("Connected");
+                PrintEstimatedTime(start);
 
-				UserUpdator.Update();
+                UserUpdator.Update();
 
-				Platform.runLater(new Runnable() {
-					@Override
-					public void run() {
-						OpenLoginPage(primaryStage);
-					}
-				});
-			}
-		});
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        OpenLoginPage(primaryStage);
+                    }
+                });
+            }
+        });
 
-		thread.start();
-		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("LoginPage/View/FirstPage.fxml"));
-		Scene scene = new Scene(loader.load());
-		scene.setFill(Color.TRANSPARENT);
-		primaryStage.initStyle(StageStyle.TRANSPARENT);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+        thread.start();
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("LoginPage/View/FirstPage.fxml"));
+        Scene scene = new Scene(loader.load());
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-	private void OpenLoginPage(Stage primaryStage) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginPage/View/Login.fxml"));
-			primaryStage.close();
-			Stage SecondaryStage = new Stage(StageStyle.TRANSPARENT);
-			Scene scene = new Scene(loader.load());
-			SecondaryStage.setScene(scene);
-			scene.setFill(Color.TRANSPARENT);
-			SecondaryStage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private void OpenLoginPage(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginPage/View/Login.fxml"));
+            primaryStage.close();
+            Stage SecondaryStage = new Stage(StageStyle.TRANSPARENT);
+            Scene scene = new Scene(loader.load());
+            SecondaryStage.setScene(scene);
+            scene.setFill(Color.TRANSPARENT);
+            SecondaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
