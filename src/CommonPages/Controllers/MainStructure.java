740728e35f660cf766ff671c060eb2f3d1a9f223
mmhlego <mmhlego@gmail.com>
2021-05-08 12:34:11 +0430

    Fixed Employee in MainStructure

--- src/CommonPages/Controllers/MainStructure.java
+++ src/CommonPages/Controllers/MainStructure.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import Controller.UserController;
+import Model.Employee.EmployeeMode;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -25,215 +26,235 @@ import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 public class MainStructure implements Initializable {
-	@FXML
-	private AnchorPane MainPanel;
-	@FXML
-	private VBox SideBar;
-	@FXML
-	private VBox menu;
-	@FXML
-	private ImageView menuImg;
-	@FXML
-	private VBox imageBox;
-	@FXML
-	private ImageView img;
+    @FXML
+    private AnchorPane MainPanel;
+    @FXML
+    private VBox SideBar;
+    @FXML
+    private VBox menu;
+    @FXML
+    private ImageView menuImg;
+    @FXML
+    private VBox imageBox;
+    @FXML
+    private ImageView img;
 
-	OpenSide sideBarController;
-	Parent root;
-	static AnchorPane main;
-	boolean HasOverlay = false, isSideOpen = false;
+    OpenSide sideBarController;
+    Parent root;
+    static AnchorPane main;
+    boolean HasOverlay = false, isSideOpen = false;
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		main = MainPanel;
-		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../Visual/OpenSide.fxml"));
-		FXMLLoader loader2;
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        main = MainPanel;
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../Visual/OpenSide.fxml"));
+        FXMLLoader loader2;
 
-		try {
-			root = loader.load();
-			sideBarController = loader.getController();
-			MainPanel.getChildren().add(root);
-			root.setTranslateX(1080);
+        try {
+            root = loader.load();
+            sideBarController = loader.getController();
+            MainPanel.getChildren().add(root);
+            root.setTranslateX(1080);
 
-			menu.setOnMouseClicked(e -> {
-				ToggleSidebar(root);
-			});
+            menu.setOnMouseClicked(e -> {
+                ToggleSidebar(root);
+            });
 
-			sideBarController.getMenuLBL().setOnMouseClicked(e -> {
-				ToggleSidebar(root);
-			});
+            sideBarController.getMenuLBL().setOnMouseClicked(e -> {
+                ToggleSidebar(root);
+            });
 
-			imageBox.setOnMouseClicked(e -> OpenLoginPage());
-			sideBarController.getExitLBL().setOnMouseClicked(e -> OpenLoginPage());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            imageBox.setOnMouseClicked(e -> OpenLoginPage());
+            sideBarController.getExitLBL().setOnMouseClicked(e -> OpenLoginPage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		root.toFront();
-		menu.setCursor(Cursor.HAND);
-		imageBox.setCursor(Cursor.HAND);
+        root.toFront();
+        menu.setCursor(Cursor.HAND);
+        imageBox.setCursor(Cursor.HAND);
 
-		AddButton("../../CommonPages/Visual/Profile.fxml", "پروفایل", "profile", true);
+        AddButton("../../CommonPages/Visual/Profile.fxml", "پروفایل", "profile", true);
 
-		if (UserController.Mode.equals(UserController.UserMode.Customer)) {
-			loader2 = new FXMLLoader(this.getClass().getResource("../../Customer/Visual/MainPage.fxml"));
+        if (UserController.Mode.equals(UserController.UserMode.Customer)) {
+            loader2 = new FXMLLoader(this.getClass().getResource("../../Customer/Visual/MainPage.fxml"));
 
-			AddButton("../../Customer/Visual/ProductsViewer.fxml", "تمام محصولات", "AllProducts", false);
-			AddButton("../../Customer/Visual/Cart.fxml", "سبد خرید", "cart", false);
-			AddButton("../../Customer/Visual/OrderHistory.fxml", "تاریخچه خرید", "OrderHistory", false);
-		} else if (UserController.Mode.equals(UserController.UserMode.Manager)) {
-			loader2 = new FXMLLoader(this.getClass().getResource("../../Manager/Visual/CustomerList.fxml"));
+            AddButton("../../Customer/Visual/ProductsViewer.fxml", "تمام محصولات", "AllProducts", false);
+            AddButton("../../Customer/Visual/Cart.fxml", "سبد خرید", "cart", false);
+            AddButton("../../Customer/Visual/OrderHistory.fxml", "تاریخچه خرید", "OrderHistory", false);
+        } else if (UserController.Mode.equals(UserController.UserMode.Manager)) {
+            loader2 = new FXMLLoader(this.getClass().getResource("../../Manager/Visual/CustomerList.fxml"));
 
-			AddButton("../../Manager/Visual/CustomerList.fxml", "لیست مشتریان", "", false);
-			AddButton("../../Manager/Visual/EmployeeList.fxml", "لیست کارکنان", "", false);
-			AddButton("../../Manager/Visual/ProductsList.fxml", "لیست کالا ها", "", false);
+            AddButton("../../Manager/Visual/CustomerList.fxml", "لیست مشتریان", "", false);
+            AddButton("../../Manager/Visual/EmployeeList.fxml", "لیست کارکنان", "", false);
+            AddButton("../../Manager/Visual/ProductsList.fxml", "لیست کالا ها", "", false);
 
-		} else {
-			loader2 = new FXMLLoader(this.getClass().getResource("../../Employee/Visual/MainPage.fxml"));
-		}
+        } else {
+            loader2 = new FXMLLoader(this.getClass().getResource("../../Employee/Visual/MainPage.fxml"));
+            if (UserController.employee.Mode.equals(EmployeeMode.ASSISTANT)) {
+                AddButton("../../Manager/Visual/CustomerList.fxml", "لیست مشتریان", "", false);
+                AddButton("../../Manager/Visual/EmployeeList.fxml", "لیست کارکنان", "", false);
+                AddButton("../../Manager/Visual/ProductsList.fxml", "لیست کالا ها", "", false);
+            } else if (UserController.employee.Mode.equals(EmployeeMode.ACCOUNTANT)) {
+                AddButton("../../Manager/Visual/EmployeeList.fxml", "لیست کارکنان", "", false);
+                AddButton("../../Manager/Visual/ProductsList.fxml", "لیست کالا ها", "", false);
+            } else if (UserController.employee.Mode.equals(EmployeeMode.QUALITYCONTROL)) {
+                AddButton("../../Manager/Visual/EmployeeList.fxml", "لیست کارکنان", "", false);
+            } else if (UserController.employee.Mode.equals(EmployeeMode.REGULAR)) {
+                AddButton("../../Manager/Visual/CustomerList.fxml", "لیست مشتریان", "", false);
+                AddButton("../../Manager/Visual/ProductsList.fxml", "لیست کالا ها", "", false);
+            } else if (UserController.employee.Mode.equals(EmployeeMode.SUPPLIER)) {
+                AddButton("../../Manager/Visual/ProductsList.fxml", "لیست کالا ها", "", false);
+            } else if (UserController.employee.Mode.equals(EmployeeMode.STOREKEEPER)) {
 
-		AddButton("../../CommonPages/Visual/Settings.fxml", "تنضیمات", "Settings", true);
+            }
+        }
 
-		try {
-			MainPanel.getChildren().add(loader2.load());
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	}
+        AddButton("../../CommonPages/Visual/Settings.fxml", "تنضیمات", "Settings", true);
 
-	private void OpenLoginPage() {
-		((Stage) MainPanel.getParent().getScene().getWindow()).close();
+        try {
+            MainPanel.getChildren().add(loader2.load());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
 
-		try {
-			Stage LoginPAge = new Stage();
-			FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../../LoginPage/View/Login.fxml"));
-			Scene scene = new Scene(loader.load());
-			scene.setFill(Color.TRANSPARENT);
-			LoginPAge.initStyle(StageStyle.TRANSPARENT);
-			LoginPAge.setScene(scene);
-			LoginPAge.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private void OpenLoginPage() {
+        ((Stage) MainPanel.getParent().getScene().getWindow()).close();
 
-	private void AddButton(String fxml, String name, String Image, boolean overlay) {
-		try {
-			FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../Visual/Component/SidePic.fxml"));
-			VBox root = loader.load();
+        try {
+            Stage LoginPAge = new Stage();
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../../LoginPage/View/Login.fxml"));
+            Scene scene = new Scene(loader.load());
+            scene.setFill(Color.TRANSPARENT);
+            LoginPAge.initStyle(StageStyle.TRANSPARENT);
+            LoginPAge.setScene(scene);
+            LoginPAge.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			try {
-				((ImageView) root.getChildren().get(0)).setImage(new Image(new FileInputStream(
-						new File(System.getProperty("user.dir") + "/src/pictures/icons/" + Image + ".png"))));
-			} catch (Exception e) {
-				System.out.println("Image not found (" + Image + ".png)");
-				// e.printStackTrace();
-			}
+    private void AddButton(String fxml, String name, String Image, boolean overlay) {
+        try {
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource("../Visual/Component/SidePic.fxml"));
+            VBox root = loader.load();
 
-			SideBar.getChildren().add(root);
+            try {
+                ((ImageView) root.getChildren().get(0)).setImage(new Image(new FileInputStream(
+                        new File(System.getProperty("user.dir") + "/src/pictures/icons/" + Image + ".png"))));
+            } catch (Exception e) {
+                System.out.println("Image not found (" + Image + ".png)");
+            }
 
-			Label label = new Label(name);
-			label.setStyle(
-					"-fx-alignment:center; -fx-font-family: 'B Koodak'; -fx-font-size: 20px; -fx-text-fill: white; -fx-cursor: hand;");
-			label.setPrefHeight(60);
-			label.setPrefWidth(190);
+            SideBar.getChildren().add(root);
 
-			sideBarController.getSideBar().getChildren().add(label);
-			root.setOnMouseClicked(e -> {
-				OpenMenuItem(fxml, overlay);
-			});
-			label.setOnMouseClicked(e -> {
-				OpenMenuItem(fxml, overlay);
-			});
+            Label label = new Label(name);
+            label.setStyle(
+                    "-fx-alignment:center; -fx-font-family: 'B Koodak'; -fx-font-size: 20px; -fx-text-fill: white; -fx-cursor: hand;");
+            label.setPrefHeight(60);
+            label.setPrefWidth(190);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            sideBarController.getSideBar().getChildren().add(label);
+            root.setOnMouseClicked(e -> {
+                OpenMenuItem(fxml, overlay);
+            });
+            label.setOnMouseClicked(e -> {
+                OpenMenuItem(fxml, overlay);
+            });
 
-	private void OpenMenuItem(String fxml, boolean overlay) {
-		if (HasOverlay && overlay) {
-			RemoveLastPage();
-		} else if (!overlay) {
-			RemoveLastPage();
-		}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		HasOverlay = overlay;
+    private void OpenMenuItem(String fxml, boolean overlay) {
+        //        if (HasOverlay && overlay) {
+        //            RemoveLastPage();
+        //        } else if (!overlay) {
+        //            RemoveLastPage();
+        //        }
+        //
+        //        HasOverlay = overlay;
 
-		if (isSideOpen) {
-			CloseSidebar();
-		}
+        if (HasOverlay) {
+            RemoveLastPage();
+        }
 
-		sideClickAction(fxml);
-	}
+        if (isSideOpen) {
+            CloseSidebar();
+        }
 
-	private void RemoveLastPage() {
-		try {
-			HasOverlay = false;
+        sideClickAction(fxml);
+    }
 
-			int len = MainPanel.getChildren().size();
+    private void RemoveLastPage() {
+        try {
+            HasOverlay = false;
 
-			if (MainPanel.getChildren().indexOf(root) == len - 1) {
-				MainPanel.getChildren().remove(len - 2);
-			} else {
-				MainPanel.getChildren().remove(len - 1);
-			}
+            int len = MainPanel.getChildren().size();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            if (MainPanel.getChildren().indexOf(root) == len - 1) {
+                MainPanel.getChildren().remove(len - 2);
+            } else {
+                MainPanel.getChildren().remove(len - 1);
+            }
 
-	public static Object addPage(String url) {
-		Object Controller = null;
-		try {
-			FXMLLoader loader = new FXMLLoader(new File(url).toURI().toURL());
-			main.getChildren().add(loader.load());
-			Controller = loader.getController();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return Controller;
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void sideClickAction(String fxml) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
-			/*
-			 * if (MainPanel.getChildren().size() >= 2) {
-			 * MainPanel.getChildren().remove(MainPanel.getChildren().size() - 1); }
-			 */
-			MainPanel.getChildren().add(loader.load());
-			root.toFront();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static Object addPage(String url) {
+        Object Controller = null;
+        try {
+            FXMLLoader loader = new FXMLLoader(new File(url).toURI().toURL());
+            main.getChildren().add(loader.load());
+            Controller = loader.getController();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Controller;
+    }
 
-	private void ToggleSidebar(Parent root) {
-		root.toFront();
-		if (!isSideOpen) {
-			OpenSidebar();
-		} else {
-			CloseSidebar();
-		}
-	}
+    private void sideClickAction(String fxml) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+            /*
+             * if (MainPanel.getChildren().size() >= 2) {
+             * MainPanel.getChildren().remove(MainPanel.getChildren().size() - 1); }
+             */
+            MainPanel.getChildren().add(loader.load());
+            root.toFront();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void OpenSidebar() {
-		TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), root);
-		SideBar.toFront();
-		transition.setCycleCount(1);
-		transition.setByX(-1080);
-		transition.play();
-		isSideOpen = true;
-	}
+    private void ToggleSidebar(Parent root) {
+        root.toFront();
+        if (!isSideOpen) {
+            OpenSidebar();
+        } else {
+            CloseSidebar();
+        }
+    }
 
-	private void CloseSidebar() {
-		TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), root);
-		SideBar.toFront();
-		transition.setCycleCount(1);
-		transition.setByX(1080);
-		transition.play();
-		isSideOpen = false;
-	}
+    private void OpenSidebar() {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), root);
+        SideBar.toFront();
+        transition.setCycleCount(1);
+        transition.setByX(-1080);
+        transition.play();
+        isSideOpen = true;
+    }
+
+    private void CloseSidebar() {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), root);
+        SideBar.toFront();
+        transition.setCycleCount(1);
+        transition.setByX(1080);
+        transition.play();
+        isSideOpen = false;
+    }
 }
