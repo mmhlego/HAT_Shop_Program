981ddd1209f88641d836f50fdfffbdc8bf90efa8
KamyabTabani <k.tabani82@gmail.com>
2021-05-13 14:52:21 +0430

    Product Information

--- src/Manager/Controller/ProductEditInformation.java
+++ src/Manager/Controller/ProductEditInformation.java
@@ -1,14 +1,22 @@
 package Manager.Controller;
 
+import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.ResourceBundle;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 
+import DataController.DBConnector;
 import Model.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -19,7 +27,7 @@ import javafx.scene.control.cell.MapValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class ProductEditInformation {
+public class ProductEditInformation implements Initializable {
 
 	@FXML
 	private ImageView ProductIMG;
@@ -28,7 +36,7 @@ public class ProductEditInformation {
 	private Button EditDataBTN;
 
 	@FXML
-	private Label ProductNameLBL;
+	private JFXTextField ProductNameLBL;
 
 	@FXML
 	private TextField ProductPriceLBL;
@@ -99,6 +107,40 @@ public class ProductEditInformation {
 		 * SpecialIMG.setVisible(special); SpecialTXT.setVisible(special);
 		 */
 
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		AddToAmountBTN.setOnAction((e) -> {
+			if (AddToAmountLBL.getText().equals("")) {
+				Alert(AlertType.ERROR, "عددی را وارد نمایید", "خطا");
+			}else if(Integer.parseInt(AddToAmountLBL.getText()) < 0){
+				Alert(AlertType.ERROR, "عدد وارد شده نمیتواند منفی باشد ! ", "خطا");
+			} 
+			else {
+				ProductAmountLBL.setText(Integer.toString(
+						(Integer.parseInt(ProductAmountLBL.getText()) + Integer.parseInt(AddToAmountLBL.getText()))));
+				AddToAmountLBL.setText("");
+			}
+		});
+
+		EditProductDataBTN.setOnAction((e) -> {
+			try {
+				PreparedStatement ps = DBConnector.Con.prepareStatement("UPDATE Products SET");
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		});
+
+	}
+
+	private void Alert(AlertType AlertType, String Content, String Title) {
+		Alert alert = new Alert(AlertType);
+		alert.setTitle(Title);
+		alert.setHeaderText(null);
+		alert.setContentText(Content);
+		alert.show();
 	}
 
 }
