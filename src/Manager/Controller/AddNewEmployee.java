9851ab25897cb61fb5a369169bbd8811b98d168e
KamyabTabani <k.tabani82@gmail.com>
2021-05-14 16:28:31 +0430

    Add Employee Completely Finished

--- src/Manager/Controller/AddNewEmployee.java
+++ src/Manager/Controller/AddNewEmployee.java
@@ -4,10 +4,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
+
+import DataController.DataAdder;
+import Model.Employee;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
@@ -49,9 +54,61 @@ public class AddNewEmployee implements Initializable {
             ((AnchorPane) BackBTN.getParent().getParent().getParent()).getChildren()
                     .remove(BackBTN.getParent().getParent());
         });
-        //Tooltip tooltip = new Tooltip("Avalible Categories: " + Product.ACCESSORIES + " ," + Product.BOOK + " ,"
-        //        + Product.COMPUTER + " ," + Product.PHONE + " ," + Product.SUPERMARKET);
-        //tooltip.setAutoHide(false);
-        //ProductCategoryTF.setTooltip(tooltip);
+        Tooltip tooltip = new Tooltip(
+                "Avalible Levels: " + Employee.EmployeeMode.ACCOUNTANT + " ," + Employee.EmployeeMode.ASSISTANT + " ,"
+                        + Employee.EmployeeMode.QUALITYCONTROL + " ," + Employee.EmployeeMode.REGULAR + " ,"
+                        + Employee.EmployeeMode.STOREKEEPER + " ," + Employee.EmployeeMode.SUPPLIER);
+        tooltip.setAutoHide(false);
+        EmployeeAccessLevelTF.setTooltip(tooltip);
+
+        AddEmployeeBTN.setOnAction((e) -> {
+            if (!IsAllFieldsComplete()) {
+                Alert(AlertType.ERROR, "بعضی از فیلد ها خالی هستند !", "خطا");
+            } else {
+                DataAdder.AddEmployee(EmployeeFirstNameTF.getText(), EmployeeLastNameTF.getText(),
+                        EmployeeUsernameTF.getText(), EmployyeePasswordTF.getText(),
+                        StringModeToIntMode(EmployeeAccessLevelTF.getText()), EmployeePhoneTF.getText(),
+                        EmployeeEMailTF.getText(),
+                        Employee.GenerateID(Employee.IntToMode(StringModeToIntMode(EmployeeAccessLevelTF.getText()))));
+                        Alert(AlertType.INFORMATION, "کارمند با موفقیت افزوده شد", "موفق");
+            }
+        });
     }
+
+    private void Alert(AlertType AlertType, String Content, String Title) {
+        Alert alert = new Alert(AlertType);
+        alert.setTitle(Title);
+        alert.setHeaderText(null);
+        alert.setContentText(Content);
+        alert.showAndWait();
+    }
+
+    private boolean IsAllFieldsComplete() {
+        if (EmployeeFirstNameTF.getText().equals("") || EmployeeLastNameTF.getText().equals("")
+                || EmployeeUsernameTF.getText().equals("") || EmployyeePasswordTF.getText().equals("")
+                || EmployeePhoneTF.getText().equals("") || EmployeeEMailTF.getText().equals("")
+                || EmployeeAccessLevelTF.getText().equals("")) {
+            return false;
+        }
+        return true;
+    }
+
+    private int StringModeToIntMode(String Mode) {
+        switch (Mode) {
+            case "ASSISTANT":
+                return 0;
+            case "ACCOUNTANT":
+                return 1;
+            case "QUALITYCONTROL":
+                return 2;
+            case "REGULAR":
+                return 3;
+            case "SUPPLIER":
+                return 4;
+            case "STOREKEEPER":
+                return 5;
+        }
+        return -1;
+    }
+
 }
