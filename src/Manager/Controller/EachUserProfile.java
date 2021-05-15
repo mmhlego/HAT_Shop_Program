88c4d36da553de98c5a5f96152701e8a165b4bee
pouyaafraz123 <pouyaafraz123@gmail.com>
2021-05-15 13:23:07 +0330

    modes are completet and tolltip finished

--- src/Manager/Controller/EachUserProfile.java
+++ src/Manager/Controller/EachUserProfile.java
@@ -1,188 +1,200 @@
 package Manager.Controller;
 
 import java.net.URL;
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
 import com.jfoenix.controls.JFXButton;
+
+import Controller.UserController;
+import Controller.UserController.UserMode;
 import DataController.DBConnector;
 import DataController.UserUpdator;
 import Model.Customer;
-import Model.Employee;
 import Model.Customer.CustomerMode;
+import Model.Employee;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Alert;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 public class EachUserProfile implements Initializable {
-    @FXML
-    private ImageView BackBTN;
-    @FXML
-    private TextField FirstNameLBL;
-    @FXML
-    private TextField UsernameLBL;
-    @FXML
-    private TextField EmailLBL;
-    @FXML
-    private Label mode;
-    @FXML
-    private TextField ModeLBL;
-    @FXML
-    private TextField LastNameLBL;
-    @FXML
-    private TextField PhoneLBL;
-    @FXML
-    private TextField IDLBL;
-    @FXML
-    private Group ValueGRP;
-    @FXML
-    private TextField ValueLBL;
-    @FXML
-    private Group AddressGRP;
-    @FXML
-    private TextField AddressLBL;
-    @FXML
-    private JFXButton EditUserInformationBTN;
-    @FXML
-    private JFXButton BanUserBTN;
-    @FXML
-    private JFXButton UnbanUserBTN;
-    @FXML
-    private JFXButton FireEmployeeBTN;
+	@FXML
+	private ImageView BackBTN;
+	@FXML
+	private TextField FirstNameLBL;
+	@FXML
+	private TextField UsernameLBL;
+	@FXML
+	private TextField EmailLBL;
+	@FXML
+	private Label mode;
+	@FXML
+	private TextField ModeLBL;
+	@FXML
+	private TextField LastNameLBL;
+	@FXML
+	private TextField PhoneLBL;
+	@FXML
+	private TextField IDLBL;
+	@FXML
+	private Group ValueGRP;
+	@FXML
+	private TextField ValueLBL;
+	@FXML
+	private Group AddressGRP;
+	@FXML
+	private TextField AddressLBL;
+	@FXML
+	private JFXButton EditUserInformationBTN;
+	@FXML
+	private JFXButton BanUserBTN;
+	@FXML
+	private JFXButton UnbanUserBTN;
+	@FXML
+	private JFXButton FireEmployeeBTN;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        BackBTN.setOnMouseClicked(e -> {
-            ((AnchorPane) BackBTN.getParent().getParent().getParent()).getChildren()
-                    .remove(BackBTN.getParent().getParent());
-        });
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		BackBTN.setOnMouseClicked(e -> {
+			((AnchorPane) BackBTN.getParent().getParent().getParent()).getChildren()
+					.remove(BackBTN.getParent().getParent());
+		});
+	}
 
-    public void LoadProfileData(Customer c) {
-        FirstNameLBL.setText(c.FirstName);
-        LastNameLBL.setText(c.LastName);
-        IDLBL.setText(c.ID);
-        PhoneLBL.setText(c.Phone);
-        UsernameLBL.setText(c.Username);
-        EmailLBL.setText(c.Email);
-        ModeLBL.setText(String.valueOf(c.Mode));
-        ValueLBL.setText(String.valueOf(c.Value));
-        AddressLBL.setText(c.Address);
+	public void LoadProfileData(Customer c) {
+		FirstNameLBL.setText(c.FirstName);
+		LastNameLBL.setText(c.LastName);
+		IDLBL.setText(c.ID);
+		PhoneLBL.setText(c.Phone);
+		UsernameLBL.setText(c.Username);
+		EmailLBL.setText(c.Email);
+		Tooltip tooltip = new Tooltip(CustomerMode.REGULAR + " ," + CustomerMode.PREMIUM + " ,"
+				+ CustomerMode.REGULARBANNED + " ," + CustomerMode.PREMIUMBANNED);
+		tooltip.setAutoHide(false);
+		ModeLBL.setText(String.valueOf(c.Mode));
+		ModeLBL.setTooltip(tooltip);
+		ValueLBL.setText(String.valueOf(c.Value));
+		AddressLBL.setText(c.Address);
 
-        if (CustomerMode.ModeToInt(c.Mode) <= 1) {
-            BanUserBTN.setVisible(true);
-            BanUserBTN.setOnMouseClicked(e -> {
-                UserUpdator.ChangeStatus(IDLBL.getText(), 2);
-                Alert(AlertType.INFORMATION, "موفق", "کاربر مورد نظر مسدود شد");
-            });
-        } else {
-            UnbanUserBTN.setVisible(true);
-            UnbanUserBTN.setOnMouseClicked(e -> {
-                UserUpdator.ChangeStatus(IDLBL.getText(), -2);
-                Alert(AlertType.INFORMATION, "موفق", "کاربر مورد نظر رفع مسدودیت شد");
-            });
-        }
+		if (CustomerMode.ModeToInt(c.Mode) <= 1) {
+			BanUserBTN.setVisible(true);
+			BanUserBTN.setOnMouseClicked(e -> {
+				UserUpdator.ChangeStatus(IDLBL.getText(), 2);
+				Alert(AlertType.INFORMATION, "موفق", "کاربر مورد نظر مسدود شد");
+			});
+		} else {
+			UnbanUserBTN.setVisible(true);
+			UnbanUserBTN.setOnMouseClicked(e -> {
+				UserUpdator.ChangeStatus(IDLBL.getText(), -2);
+				Alert(AlertType.INFORMATION, "موفق", "کاربر مورد نظر رفع مسدودیت شد");
+			});
+		}
 
-        EditUserInformationBTN.setOnAction(e -> {
-            FirstNameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            LastNameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            Tooltip tooltip = new Tooltip("آیدی قابل تغییر نیست");
-            tooltip.setFont(Font.font("B Kamran", FontWeight.BOLD, 20));
-            tooltip.setAutoHide(false);
-            IDLBL.setTooltip(tooltip);
-            PhoneLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            UsernameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            EmailLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            ModeLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            ValueLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            AddressLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            if (EditUserInformationBTN.getText().equals("ذخیره اطلاعات کاربر")) {
-                UpdateCustomerData();
-            }
-            EditUserInformationBTN
-                    .setText(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر") ? "ذخیره اطلاعات کاربر"
-                            : "تغییر اطلاعات کاربر");
-        });
-    }
+		EditUserInformationBTN.setOnAction(e -> {
+			FirstNameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			LastNameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			Tooltip t = new Tooltip("آیدی قابل تغییر نیست");
+			t.setFont(Font.font("B Kamran", FontWeight.BOLD, 20));
+			t.setAutoHide(false);
+			IDLBL.setTooltip(t);
+			PhoneLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			UsernameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			EmailLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			ModeLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			ValueLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			AddressLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			if (EditUserInformationBTN.getText().equals("ذخیره اطلاعات کاربر")) {
+				UpdateCustomerData();
+			}
+			EditUserInformationBTN
+					.setText(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر") ? "ذخیره اطلاعات کاربر"
+							: "تغییر اطلاعات کاربر");
+		});
+	}
 
-    public void LoadProfileData(Employee u) {
-        FirstNameLBL.setText(u.FirstName);
-        LastNameLBL.setText(u.LastName);
-        IDLBL.setText(u.ID);
-        PhoneLBL.setText(u.Phone);
-        UsernameLBL.setText(u.Username);
-        EmailLBL.setText(u.Email);
-        ModeLBL.setText(String.valueOf(u.Mode));
-        AddressGRP.setVisible(false);
-        ValueGRP.setVisible(false);
+	public void LoadProfileData(Employee u) {
+		FirstNameLBL.setText(u.FirstName);
+		LastNameLBL.setText(u.LastName);
+		IDLBL.setText(u.ID);
+		PhoneLBL.setText(u.Phone);
+		UsernameLBL.setText(u.Username);
+		EmailLBL.setText(u.Email);
+		ModeLBL.setText(String.valueOf(u.Mode));
+		AddressGRP.setVisible(false);
+		ValueGRP.setVisible(false);
 
-        FireEmployeeBTN.setVisible(true);
-        FireEmployeeBTN.setOnMouseClicked(e -> {
-            UserUpdator.FireEmployee(IDLBL.getText());
-            Alert(AlertType.INFORMATION, "موفق", "کارمند مورد نظر با موفقیت حدف شد");
-        });
+		FireEmployeeBTN.setVisible(true);
+		FireEmployeeBTN.setOnMouseClicked(e -> {
+			UserUpdator.FireEmployee(IDLBL.getText());
+			Alert(AlertType.INFORMATION, "موفق", "کارمند مورد نظر با موفقیت حدف شد");
+		});
+		if (UserController.Mode.equals(UserMode.Employee)) {
+			FireEmployeeBTN.setDisable(true);
+		}
 
-        EditUserInformationBTN.setOnAction(e -> {
-            FirstNameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            LastNameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            Tooltip tooltip = new Tooltip("آیدی قابل تغییر نیست");
-            tooltip.setAutoHide(false);
-            tooltip.setFont(Font.font("B Kamran", FontWeight.BOLD, 20));
-            IDLBL.setTooltip(tooltip);
-            PhoneLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            UsernameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            EmailLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            ModeLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
-            if (EditUserInformationBTN.getText().equals("ذخیره اطلاعات کاربر")) {
-                UpdateEmployeeData();
-            }
-            EditUserInformationBTN
-                    .setText(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر") ? "ذخیره اطلاعات کاربر"
-                            : "تغییر اطلاعات کاربر");
-        });
-    }
+		EditUserInformationBTN.setOnAction(e -> {
+			FirstNameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			LastNameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			Tooltip tooltip = new Tooltip("آیدی قابل تغییر نیست");
+			tooltip.setAutoHide(false);
+			tooltip.setFont(Font.font("B Kamran", FontWeight.BOLD, 20));
+			IDLBL.setTooltip(tooltip);
+			PhoneLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			UsernameLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			EmailLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			ModeLBL.setEditable(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر"));
+			if (EditUserInformationBTN.getText().equals("ذخیره اطلاعات کاربر")) {
+				UpdateEmployeeData();
+			}
+			EditUserInformationBTN
+					.setText(EditUserInformationBTN.getText().equals("تغییر اطلاعات کاربر") ? "ذخیره اطلاعات کاربر"
+							: "تغییر اطلاعات کاربر");
+		});
+	}
 
-    private void UpdateCustomerData() {
-        try {
-            PreparedStatement ps = DBConnector.Con.prepareStatement("UPDATE Customers SET FirstName=" + "\'"
-                    + FirstNameLBL.getText() + "\' , LastName=" + "\'" + LastNameLBL.getText() + "\' , Username=" + "\'"
-                    + UsernameLBL.getText() + "\' , Phone=" + "\'" + PhoneLBL.getText() + "\' , Email=" + "\'"
-                    + EmailLBL.getText() + "\' , Address=" + "\'" + AddressLBL.getText() + "\' , Value="
-                    + Long.parseLong(ValueLBL.getText()) + " WHERE ID=" + "\'" + IDLBL.getText() + "\'");
-            ps.executeUpdate();
-            Alert(AlertType.INFORMATION, "موفق", "اطلاعات کاربر با موفقیت به روزرسانی شد !");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	private void UpdateCustomerData() {
+		try {
+			PreparedStatement ps = DBConnector.Con.prepareStatement("UPDATE Customers SET FirstName=" + "\'"
+					+ FirstNameLBL.getText() + "\' , LastName=" + "\'" + LastNameLBL.getText() + "\' , Username=" + "\'"
+					+ UsernameLBL.getText() + "\' , Phone=" + "\'" + PhoneLBL.getText() + "\' , Email=" + "\'"
+					+ EmailLBL.getText() + "\' , Address=" + "\'" + AddressLBL.getText() + "\' , Value="
+					+ Long.parseLong(ValueLBL.getText()) + " WHERE ID=" + "\'" + IDLBL.getText() + "\'");
+			ps.executeUpdate();
+			Alert(AlertType.INFORMATION, "موفق", "اطلاعات کاربر با موفقیت به روزرسانی شد !");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void UpdateEmployeeData() {
-        try {
-            PreparedStatement ps = DBConnector.Con.prepareStatement("UPDATE Employees SET FirstName=" + "\'"
-                    + FirstNameLBL.getText() + "\' , LastName=" + "\'" + LastNameLBL.getText() + "\' , Username=" + "\'"
-                    + UsernameLBL.getText() + "\' , Phone=" + "\'" + PhoneLBL.getText() + "\' , Email=" + "\'"
-                    + EmailLBL.getText() + "\'" + " WHERE ID=" + "\'" + IDLBL.getText() + "\'");
-            ps.executeUpdate();
-            Alert(AlertType.INFORMATION, "موفق", "اطلاعات کاربر با موفقیت به روزرسانی شد !");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+	private void UpdateEmployeeData() {
+		try {
+			PreparedStatement ps = DBConnector.Con.prepareStatement("UPDATE Employees SET FirstName=" + "\'"
+					+ FirstNameLBL.getText() + "\' , LastName=" + "\'" + LastNameLBL.getText() + "\' , Username=" + "\'"
+					+ UsernameLBL.getText() + "\' , Phone=" + "\'" + PhoneLBL.getText() + "\' , Email=" + "\'"
+					+ EmailLBL.getText() + "\'" + " WHERE ID=" + "\'" + IDLBL.getText() + "\'");
+			ps.executeUpdate();
+			Alert(AlertType.INFORMATION, "موفق", "اطلاعات کاربر با موفقیت به روزرسانی شد !");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void Alert(AlertType AlertType, String Title, String Content) {
-        Alert alert = new Alert(AlertType);
-        alert.setTitle(Title);
-        alert.setHeaderText(null);
-        alert.setContentText(Content);
-        alert.show();
-    }
+	private void Alert(AlertType AlertType, String Title, String Content) {
+		Alert alert = new Alert(AlertType);
+		alert.setTitle(Title);
+		alert.setHeaderText(null);
+		alert.setContentText(Content);
+		alert.show();
+	}
 
 }
--- src/Manager/Controller/ProductEditInformation.java
+++ src/Manager/Controller/ProductEditInformation.java
@@ -6,18 +6,22 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 
+import Controller.UserController;
+import Controller.UserController.UserMode;
 import DataController.DBConnector;
+import Model.Employee.EmployeeMode;
 import Model.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -28,111 +32,122 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class ProductEditInformation implements Initializable {
-    @FXML
-    private ImageView ProductIMG;
-    @FXML
-    private Button EditDataBTN;
-    @FXML
-    private JFXTextField ProductNameLBL;
-    @FXML
-    private TextField ProductPriceLBL;
-    @FXML
-    private TextField ProductCategoryLBL;
-    @FXML
-    private TextField ProductIDLBL;
-    @FXML
-    private ImageView SpecialIMG;
-    @FXML
-    private Label SpecialTXT;
-    @FXML
-    private Group AddToCartGroup;
-    @FXML
-    private TextField ProductAmountLBL;
-    @FXML
-    private Group AddAmountGRP;
-    @FXML
-    private Button AddToAmountBTN;
-    @FXML
-    private TextField AddToAmountLBL;
-    @FXML
-    private JFXTextArea ProductDescriptionTXT;
-    @FXML
-    private TableView<Map<String, Object>> ProductDetailsTable;
-    @SuppressWarnings("rawtypes")
-    @FXML
-    private TableColumn<Map, String> InformationColumn;
-    @SuppressWarnings("rawtypes")
-    @FXML
-    private TableColumn<Map, String> MColumn;
-    @FXML
-    private Button EditProductDataBTN;
-    @FXML
-    private Button DeleteProductBTN;
+	@FXML
+	private ImageView ProductIMG;
+	@FXML
+	private Button EditDataBTN;
+	@FXML
+	private JFXTextField ProductNameLBL;
+	@FXML
+	private TextField ProductPriceLBL;
+	@FXML
+	private TextField ProductCategoryLBL;
+	@FXML
+	private TextField ProductIDLBL;
+	@FXML
+	private ImageView SpecialIMG;
+	@FXML
+	private Label SpecialTXT;
+	@FXML
+	private Group AddToCartGroup;
+	@FXML
+	private TextField ProductAmountLBL;
+	@FXML
+	private Group AddAmountGRP;
+	@FXML
+	private Button AddToAmountBTN;
+	@FXML
+	private TextField AddToAmountLBL;
+	@FXML
+	private JFXTextArea ProductDescriptionTXT;
+	@FXML
+	private TableView<Map<String, Object>> ProductDetailsTable;
+	@SuppressWarnings("rawtypes")
+	@FXML
+	private TableColumn<Map, String> InformationColumn;
+	@SuppressWarnings("rawtypes")
+	@FXML
+	private TableColumn<Map, String> MColumn;
+	@FXML
+	private Button EditProductDataBTN;
+	@FXML
+	private Button DeleteProductBTN;
 
-    public void ShowProductInfo(Product p, Image image, boolean special) {
-        ProductAmountLBL.setText(String.valueOf(p.Amount));
-        ObservableList<Map<String, Object>> items = FXCollections.<Map<String, Object>>observableArrayList();
-        for (int j = 0; j < p.Details.length; j++) {
-            Map<String, Object> item = new HashMap<>();
-            item.put("firstrow", p.Details[j][0]);
-            item.put("secondrow", p.Details[j][1]);
-            items.add(item);
-        }
-        MColumn.setCellValueFactory(new MapValueFactory<String>("firstrow"));
-        InformationColumn.setCellValueFactory(new MapValueFactory<String>("secondrow"));
-        ProductDetailsTable.setItems(items);
-        ProductIMG.setImage(image);
-        ProductCategoryLBL.setText(p.Category);
-        ProductDescriptionTXT.setText(p.Description);
-        ProductIDLBL.setText(p.ID);
-        ProductNameLBL.setText(p.Name);
-        ProductPriceLBL.setText(String.valueOf(p.Price));
-        /*
-         * SpecialIMG.setVisible(special); SpecialTXT.setVisible(special);
-         */
-        DeleteProductBTN.setOnMouseClicked(e -> {
-            //TODO Delete Product
-        });
-    }
+	public void ShowProductInfo(Product p, Image image, boolean special) {
+		ProductAmountLBL.setText(String.valueOf(p.Amount));
+		ObservableList<Map<String, Object>> items = FXCollections.<Map<String, Object>>observableArrayList();
+		for (int j = 0; j < p.Details.length; j++) {
+			Map<String, Object> item = new HashMap<>();
+			item.put("firstrow", p.Details[j][0]);
+			item.put("secondrow", p.Details[j][1]);
+			items.add(item);
+		}
+		MColumn.setCellValueFactory(new MapValueFactory<String>("firstrow"));
+		InformationColumn.setCellValueFactory(new MapValueFactory<String>("secondrow"));
+		ProductDetailsTable.setItems(items);
+		ProductIMG.setImage(image);
+		ProductCategoryLBL.setText(p.Category);
+		ProductDescriptionTXT.setText(p.Description);
+		ProductIDLBL.setText(p.ID);
+		ProductNameLBL.setText(p.Name);
+		ProductPriceLBL.setText(String.valueOf(p.Price));
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+		/*
+		 * SpecialIMG.setVisible(special); SpecialTXT.setVisible(special);
+		 */
+		DeleteProductBTN.setOnMouseClicked(e -> {
+			// TODO Delete Product
+		});
+	}
 
-        AddToAmountBTN.setOnAction((e) -> {
-            if (AddToAmountLBL.getText().equals("")) {
-                Alert(AlertType.ERROR, "عددی را وارد نمایید", "خطا");
-            } else if (Integer.parseInt(AddToAmountLBL.getText()) < 0) {
-                Alert(AlertType.ERROR, "عدد وارد شده نمیتواند منفی باشد ! ", "خطا");
-            } else {
-                ProductAmountLBL.setText(Integer.toString(
-                        (Integer.parseInt(ProductAmountLBL.getText()) + Integer.parseInt(AddToAmountLBL.getText()))));
-                AddToAmountLBL.setText("");
-            }
-        });
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 
-        EditProductDataBTN.setOnAction((e) -> {
-            try {
-                PreparedStatement ps = DBConnector.Con.prepareStatement("UPDATE Products SET Name=" + "\'"
-                        + ProductNameLBL.getText() + "\'" + ", Description=" + "\'" + ProductDescriptionTXT.getText()
-                        + "\'" + ", Price=" + Long.parseLong(ProductPriceLBL.getText()) + ", Category=" + "\'"
-                        + ProductCategoryLBL.getText() + "\'" + ", Amount="
-                        + Integer.parseInt(ProductAmountLBL.getText()) + " WHERE ID=" + "\'" + ProductIDLBL.getText()
-                        + "\'");
-                ps.executeUpdate();
-                Alert(AlertType.INFORMATION, "اطلاعات کالا با موفقیت تغییر یافت", "موفق");
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        });
+		AddToAmountBTN.setOnAction((e) -> {
+			if (AddToAmountLBL.getText().equals("")) {
+				Alert(AlertType.ERROR, "عددی را وارد نمایید", "خطا");
+			} else if (Integer.parseInt(AddToAmountLBL.getText()) < 0) {
+				Alert(AlertType.ERROR, "عدد وارد شده نمیتواند منفی باشد ! ", "خطا");
+			} else {
+				ProductAmountLBL.setText(Integer.toString(
+						(Integer.parseInt(ProductAmountLBL.getText()) + Integer.parseInt(AddToAmountLBL.getText()))));
+				AddToAmountLBL.setText("");
+			}
+		});
 
-    }
+		if (UserController.Mode.equals(UserMode.Employee)
+				&& UserController.employee.Mode.equals(EmployeeMode.ACCOUNTANT)) {
+			ProductCategoryLBL.setEditable(false);
+			ProductDescriptionTXT.setEditable(false);
+			ProductIDLBL.setEditable(false);
+			ProductAmountLBL.setEditable(false);
+			AddToAmountBTN.setDisable(true);
 
-    private void Alert(AlertType AlertType, String Content, String Title) {
-        Alert alert = new Alert(AlertType);
-        alert.setTitle(Title);
-        alert.setHeaderText(null);
-        alert.setContentText(Content);
-        alert.show();
-    }
+		}
+
+		EditProductDataBTN.setOnAction((e) -> {
+			try {
+				PreparedStatement ps = DBConnector.Con.prepareStatement("UPDATE Products SET Name=" + "\'"
+						+ ProductNameLBL.getText() + "\'" + ", Description=" + "\'" + ProductDescriptionTXT.getText()
+						+ "\'" + ", Price=" + Long.parseLong(ProductPriceLBL.getText()) + ", Category=" + "\'"
+						+ ProductCategoryLBL.getText() + "\'" + ", Amount="
+						+ Integer.parseInt(ProductAmountLBL.getText()) + " WHERE ID=" + "\'" + ProductIDLBL.getText()
+						+ "\'");
+				ps.executeUpdate();
+				Alert(AlertType.INFORMATION, "اطلاعات کالا با موفقیت تغییر یافت", "موفق");
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		});
+
+	}
+
+	private void Alert(AlertType AlertType, String Content, String Title) {
+		Alert alert = new Alert(AlertType);
+		alert.setTitle(Title);
+		alert.setHeaderText(null);
+		alert.setContentText(Content);
+		alert.show();
+	}
 
 }
