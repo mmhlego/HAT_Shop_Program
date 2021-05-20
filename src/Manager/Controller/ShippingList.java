487c1a97e27d07be17fc062523fbbadc4cdc5c5f
mmhlego <mmhlego@gmail.com>
2021-05-20 13:59:07 +0430

    Lists Bug Fixes

--- src/Manager/Controller/ShippingList.java
+++ src/Manager/Controller/ShippingList.java
@@ -16,66 +16,72 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class ShippingList implements Initializable {
-	@FXML
-	private VBox MainPanel;
-	@FXML
-	private TextField SearchTXF;
-	@FXML
-	private Button SearchBTN;
-	@FXML
-	private Button RefreshPageBTN;
+    @FXML
+    private VBox MainPanel;
+    @FXML
+    private TextField SearchTXF;
+    @FXML
+    private Button SearchBTN;
+    @FXML
+    private Button RefreshPageBTN;
 
-	ArrayList<Shipping> AllShippings;
+    ArrayList<Shipping> AllShippings;
 
-	int i = 0;
+    int i = 0;
 
-	private void search(String s) throws Exception {
-		ArrayList<Shipping> searchResult = new ArrayList<>();
-		for (Shipping shipping : AllShippings) {
-			if (shipping.Date.toString().toLowerCase().contains(s)
-					|| String.valueOf(shipping.Fee).toLowerCase().contains(s) || shipping.ID.toLowerCase().contains(s)
-					|| shipping.OrderId.toLowerCase().contains(s)
-					|| String.valueOf(ShippingMode.IntToMode(shipping.Status)).toLowerCase().contains(s)) {
-				searchResult.add(shipping);
-			}
-		}
-		ShowAllShipping(searchResult);
-	}
+    private void search(String s) throws Exception {
+        ArrayList<Shipping> searchResult = new ArrayList<>();
+        for (Shipping shipping : AllShippings) {
+            if (shipping.Date.toString().toLowerCase().contains(s)
+                    || String.valueOf(shipping.Fee).toLowerCase().contains(s) || shipping.ID.toLowerCase().contains(s)
+                    || shipping.OrderId.toLowerCase().contains(s)
+                    || String.valueOf(ShippingMode.IntToMode(shipping.Status)).toLowerCase().contains(s)) {
+                searchResult.add(shipping);
+            }
+        }
+        ShowAllShipping(searchResult);
+    }
 
-	private void ShowAllShipping(ArrayList<Shipping> shippings) throws Exception {
-		for (Shipping shipping : shippings) {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("../Components/EachShipping.fxml"));
-			Parent root = loader.load();
-			EachShipping c = loader.getController();
-			c.ShowShipping(shipping);
-			MainPanel.getChildren().add(root);
-		}
+    private void ShowAllShipping(ArrayList<Shipping> shippings) throws Exception {
+        MainPanel.getChildren().clear();
 
-	}
+        if (shippings.size() == 0) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Components/NoResults.fxml"));
+            MainPanel.getChildren().add(loader.load());
+        }
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		try {
-			AllShippings = UserGetter.ConvertShippingToArrayList(UserGetter.GetShippingsDB());
-			ShowAllShipping(AllShippings);
-			SearchBTN.setOnAction(e -> {
-				try {
-					search(SearchTXF.getText().toLowerCase());
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-			});
-			RefreshPageBTN.setOnAction(e -> {
-				try {
-					AllShippings = UserGetter.ConvertShippingToArrayList(UserGetter.GetShippingsDB());
-					search(SearchTXF.getText().toLowerCase());
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        for (Shipping shipping : shippings) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Components/EachShipping.fxml"));
+            Parent root = loader.load();
+            EachShipping c = loader.getController();
+            c.ShowShipping(shipping);
+            MainPanel.getChildren().add(root);
+        }
+    }
 
-	}
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        try {
+            AllShippings = UserGetter.ConvertShippingToArrayList(UserGetter.GetShippingsDB());
+            ShowAllShipping(AllShippings);
+            SearchBTN.setOnAction(e -> {
+                try {
+                    search(SearchTXF.getText().toLowerCase());
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            });
+            RefreshPageBTN.setOnAction(e -> {
+                try {
+                    AllShippings = UserGetter.ConvertShippingToArrayList(UserGetter.GetShippingsDB());
+                    search(SearchTXF.getText().toLowerCase());
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/Manager/Controller/TransactionList.java
+++ src/Manager/Controller/TransactionList.java
@@ -15,66 +15,72 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 public class TransactionList implements Initializable {
-	@FXML
-	private VBox MainPanel;
-	@FXML
-	private TextField SearchTXF;
-	@FXML
-	private Button SearchBTN;
-	@FXML
-	private Button RefreshPageBTN;
+    @FXML
+    private VBox MainPanel;
+    @FXML
+    private TextField SearchTXF;
+    @FXML
+    private Button SearchBTN;
+    @FXML
+    private Button RefreshPageBTN;
 
-	ArrayList<Transaction> AllTransactions;
+    ArrayList<Transaction> AllTransactions;
 
-	int i = 0;
+    int i = 0;
 
-	private void search(String s) throws Exception {
-		ArrayList<Transaction> searchResult = new ArrayList<>();
-		for (Transaction transaction : AllTransactions) {
-			if (String.valueOf(transaction.Amount).toLowerCase().contains(s)
-					|| transaction.Date.toString().toLowerCase().contains(s)
-					|| transaction.FromID.toLowerCase().contains(s) || transaction.ID.toLowerCase().contains(s)) {
-				searchResult.add(transaction);
-			}
+    private void search(String s) throws Exception {
+        System.out.println(s);
+        ArrayList<Transaction> searchResult = new ArrayList<>();
+        for (Transaction transaction : AllTransactions) {
+            if (String.valueOf(transaction.Amount).toLowerCase().contains(s)
+                    || transaction.Date.toString().toLowerCase().contains(s)
+                    || transaction.FromID.toLowerCase().contains(s) || transaction.ID.toLowerCase().contains(s)) {
+                searchResult.add(transaction);
+            }
+        }
+        System.out.println(searchResult.size());
+        ShowAllTransactions(searchResult);
+    }
 
-		}
-		ShowAllTransactions(searchResult);
-	}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            AllTransactions = UserGetter.ConvertTransactionToArrayList(UserGetter.GetTransactionsDB());
+            ShowAllTransactions(AllTransactions);
+            SearchBTN.setOnAction(e -> {
+                try {
+                    search(SearchTXF.getText().toLowerCase());
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            });
+            RefreshPageBTN.setOnAction(e -> {
+                try {
+                    AllTransactions = UserGetter.ConvertTransactionToArrayList(UserGetter.GetTransactionsDB());
+                    search(SearchTXF.getText().toLowerCase());
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		try {
-			AllTransactions = UserGetter.ConvertTransactionToArrayList(UserGetter.GetTransactionsDB());
-			ShowAllTransactions(AllTransactions);
-			SearchBTN.setOnAction(e -> {
-				try {
-					search(SearchTXF.getText().toLowerCase());
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-			});
-			RefreshPageBTN.setOnAction(e -> {
-				try {
-					AllTransactions = UserGetter.ConvertTransactionToArrayList(UserGetter.GetTransactionsDB());
-					search(SearchTXF.getText().toLowerCase());
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private void ShowAllTransactions(ArrayList<Transaction> transactions) throws Exception {
+        MainPanel.getChildren().clear();
 
-	private void ShowAllTransactions(ArrayList<Transaction> transactions) throws Exception {
-		for (Transaction transaction : transactions) {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("../Components/EachTransaction.fxml"));
-			Parent root = loader.load();
-			EachTransaction c = loader.getController();
-			c.ShowTransactions(transaction);
-			MainPanel.getChildren().add(root);
-		}
+        if (transactions.size() == 0) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Components/NoResults.fxml"));
+            MainPanel.getChildren().add(loader.load());
+        }
 
-	}
-
+        for (Transaction transaction : transactions) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../Components/EachTransaction.fxml"));
+            Parent root = loader.load();
+            EachTransaction c = loader.getController();
+            c.ShowTransactions(transaction);
+            MainPanel.getChildren().add(root);
+        }
+    }
 }
